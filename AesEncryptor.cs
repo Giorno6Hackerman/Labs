e90fe585ee161dd52e80df445aa68741212d0597
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-05-21 02:02:10 +0300

    second encryption add

---
+++ AesEncryptor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace AesEncryption
+{
+    public class AesEncryptor
+    {
+        static private Aes crypty = Aes.Create();
+        static private ICryptoTransform encryptor = crypty.CreateEncryptor();
+        static private ICryptoTransform decryptor = crypty.CreateDecryptor();
+
+        public AesEncryptor()
+        {
+            
+        }
+
+
+
+        public void Encrypt(Stream data, string fileName)
+        {
+            //FileStream file = File.Create(fileName);
+            byte[] encrypted = new byte[256];
+
+            using (FileStream file = File.Create(fileName))
+            {
+                using (BinaryReader reader = new BinaryReader(data))
+                {
+                    int count = reader.Read(encrypted, 0, encrypted.Length);
+                    while (count > 0)
+                    {
+                        byte[] result = encryptor.TransformFinalBlock(encrypted, 0, count);
+                        file.Write(result, 0, result.Length);
+                        count = reader.Read(encrypted, 0, encrypted.Length);
+                    }
+                }
+            }
+        }
+
+
+        public void Decrypt(Stream data, string fileName)
+        {
+            //StreamReader reader = new StreamReader(fileName);
+            byte[] decrypted = new byte[256];
+
+            using (FileStream file = File.OpenRead(fileName))
+            {
+                BinaryWriter writer = new BinaryWriter(data);
+                int count = file.Read(decrypted, 0, decrypted.Length);
+                while (count > 0)
+                {
+                    byte[] result = decryptor.TransformFinalBlock(decrypted, 0, count);
+                    writer.Write(result, 0, result.Length);
+                    count = file.Read(decrypted, 0, decrypted.Length);
+                }
+                writer.Seek(0, SeekOrigin.Begin);
+
+            }
+        }
+    }
+}
